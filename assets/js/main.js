57a3c0f27e088cd9e26f10428681a15b471a33c1
AshlynAustin <ae.austin864@gmail.com>
2026-03-31 17:52:41 +0000

    Save everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,11 @@
-// main.js
+let rotate = 0;
 
-// create a variable to store updates to rotation
-let rotation = 0;
-// add a click listener to the whole document
 document.addEventListener("click", function () {
-    // variables to store random min/max values
+
     let min = 10;
     let max = 40;
-    // update rotation
-    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
-    // apply rotation
+
+    rotation += Math,floor(Math,random() * (max - min + 1)) + min;
+
     document.body.style.transform = `rotate(${rotation}deg)`;
 });
