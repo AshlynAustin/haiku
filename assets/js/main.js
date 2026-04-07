fcf7dd148595487dea26d2d30c428fd0ef4d7662
AshlynAustin <ae.austin864@gmail.com>
2026-04-07 23:58:57 +0000

    Save everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
-let rotate = 0;
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1 ) + min;
+}
 
-document.addEventListener("click", function () {
+let divs = document.querySelectorAll(".grid-container div");
 
-    let min = 10;
-    let max = 40;
-
-    rotation += Math,floor(Math,random() * (max - min + 1)) + min;
-
-    document.body.style.transform = `rotate(${rotation}deg)`;
-});
+function randomize() {
+    divs.forEach(function (div) {
+        let scale = randomNumber(0.5, 1.2);
+        let translateX = randomNumber(0,50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    }
+);
+    console.log(divs.length, "randomize!");
+}
+document.addEventListener("click", randomize);
